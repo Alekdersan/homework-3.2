fc1f868f8ab7df039588b18906fa13a03bda9a49
alekdersan <repka1795@gmail.com>
2022-07-04 23:28:54 +0300

    Задание по теме 4.1 Продолжение знакомства с SQL.

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -12,4 +12,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     Set<Student> findByAgeBetween(Integer minAge, Integer maxAge);
 
     Set<Student> findByNameContainsIgnoreCase(String part);
+
+
 }
